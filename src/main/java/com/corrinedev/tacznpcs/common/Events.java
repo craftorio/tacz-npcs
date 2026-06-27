ff015736bfa31919123266e9d4f42a4f71a94343
scherepanov <s@cherepanov.co>
2026-06-27 23:28:18 +0400

    Make monsters retaliate when NPCs shoot or hurt them.
    Bump version to 1.0.3 so TACZ gun hits reliably aggro monsters onto bandits and duty NPCs via TargetLock.

--- src/main/java/com/corrinedev/tacznpcs/common/Events.java
+++ src/main/java/com/corrinedev/tacznpcs/common/Events.java
@@ -2,13 +2,15 @@ package com.corrinedev.tacznpcs.common;
 
 import com.corrinedev.tacznpcs.Config;
 import com.corrinedev.tacznpcs.common.entity.AbstractScavEntity;
+import com.corrinedev.tacznpcs.common.entity.behavior.TargetLock;
 import com.tacz.guns.api.event.common.EntityHurtByGunEvent;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
-import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -22,6 +24,30 @@ public class Events {
             event.setBaseAmount((float) (event.getBaseAmount() * Config.NPCDAMAGEPLAYER.get()));
         } else if (event.getAttacker() instanceof AbstractScavEntity) {
             event.setBaseAmount((float) (event.getBaseAmount() * Config.NPCDAMAGE.get()));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onMonsterShotByScav(EntityHurtByGunEvent.Post event) {
+        if (event.getHurtEntity() instanceof Monster monster) {
+            retaliateMonster(monster, event.getAttacker());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onMonsterHurtByScav(LivingHurtEvent event) {
+        if (event.getEntity().level().isClientSide() || !(event.getEntity() instanceof Monster monster)) {
+            return;
+        }
+        Entity source = event.getSource().getEntity();
+        if (source instanceof LivingEntity attacker) {
+            retaliateMonster(monster, attacker);
+        }
+    }
+
+    private static void retaliateMonster(Monster monster, LivingEntity attacker) {
+        if (attacker instanceof AbstractScavEntity scav && scav.isAlive() && !scav.deadAsContainer) {
+            TargetLock.retaliate(monster, scav);
         }
     }
     @SubscribeEvent
